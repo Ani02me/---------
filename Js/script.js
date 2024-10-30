601fb30dd0cc9c77a52366bc89d7d1ccdeb62504
Elmura <erbakytelmura@gmail.com>
2024-10-31 02:12:26 +0500

    Home work

---
+++ Js/script.js
@@ -0,0 +1,26 @@
+let name = "Elmura"
+
+console.log("My name:"+name +"/Type:"+typeof name );
+
+
+const yerth =26;
+
+console.log("My yerth:"+yerth+"/Type:"+typeof yerth);
+
+
+let score=false
+if (true) {
+    let score=true
+    console.log("His a Student?:"+score+"/Type:"+typeof score);
+
+
+}
+
+var aga="Жасы:"
+var aga="16"
+console.log("Жасы:" +aga+"/type:"+typeof aga);
+
+var aga="Жасы:"
+var aga="1"
+var aga="6"
+console.log("Жасы:"+"1" + "6" + "/Type:"+typeof aga);
